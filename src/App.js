a843088883f00c67bec2bf2170a32dfbe416e229
Alan Mc Gee <mcgee.al@gmail.com>
2020-05-25 14:20:02 +0100

    simple function call demo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const App = () =>{
+  const message ="Reacts the bomb"
+
+  const handleClick = () =>{
+    alert('You clicked the message')
+  }
   return (
     <div>
-      <h1>hello world!! test
-        
+      <h1>hello world!! 
       </h1>
+  <h2 onClick={handleClick}>Click {message} here</h2>
     </div>
   )
 }
