48782a5c628495510af5ab6179872ce16e675aa9
Alan Mc Gee <mcgee.al@gmail.com>
2020-05-26 12:42:38 +0100

    Pulling from a list of objects

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React from "react";
 import "./styles.css";
 
+//App.js
 const App = () => {
+  const contacts = [
+    { name: "Jenny Han", email: "jenny.han@notreal.com", age: 25 },
+    { name: "Jason Long", email: "jason.long@notreal.com", age: 45 },
+    { name: "Peter Pan", email: "peter.pan@neverland.com", age: 100 },
+    { name: "Amy McDonald", email: "amy@email.com", age: 33 },
+  ];
+
   return (
     <>
-      <ContactCard />
-      <ContactCard />
-      <ContactCard />
+      {contacts.map((contact) => (
+        <ContactCard
+          avatar="https://via.placeholder.com/150"
+          name={contact.name}
+          email={contact.email}
+          age={contact.age}
+        />
+      ))}
     </>
-  )
-}
+  );
+};
 
-const ContactCard = () => {
-  const [showAge, setShowAge] = React.useState(true);
+const ContactCard = (props) => {
+  const [showAge, setShowAge] = React.useState(false);
+
   return (
     <div className="contact-card">
-      <img src="https://via.placeholder.com/150" alt="profile" />
+      <img src={props.avatar} alt="profile" />
       <div className="user-details">
-        <p>Name: Jenny Han</p>
-        <p>Email: Jenny.Han@notreal.com</p>
-        {
-        // {showAge === true ? <p>Age: 25</p> : null}
-        }
-        {showAge && <p>Age: 25</p> }
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={() => setShowAge(!showAge)}>Toggle Age</button>
+        {showAge && <p>Age: {props.age}</p>}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
